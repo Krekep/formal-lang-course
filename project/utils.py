99072af9c563a197877c62a0925544e43a3d0fb2
Pavel Alimov <42354122+Krekep@users.noreply.github.com>
2021-09-14 15:07:50 +0300

    [Project] Fix notes
    - Add new tests
    - Add minimize dfa
    - Fix codestyle

--- project/utils.py
+++ project/utils.py
@@ -6,17 +6,32 @@ from typing import Tuple
 import cfpq_data
 import networkx as nx
 
-__all__ = ["get_graph_info", "create_two_cycle_graph"]
+import pyformlang
+from pyformlang.finite_automaton import (
+    DeterministicFiniteAutomaton,
+    NondeterministicFiniteAutomaton,
+)
+from pyformlang.regular_expression import Regex
+
+__all__ = [
+    "get_graph_info",
+    "create_two_cycle_graph",
+    "regex_to_nfa",
+    "nfa_to_minimal_dfa",
+    "regex_to_dfa",
+]
 
 
 def get_graph_info(graph: nx.MultiDiGraph) -> Tuple[int, int, set]:
     """
     Gets information about the graph as a tuple of 3 elements -
     the number of nodes, the number of edges, and labels set on the edges.
+
     Parameters
     ----------
     graph: nx.MultiDiGraph
         Graph from which information is gained
+
     Returns
     -------
     Tuple[int, int, set]
@@ -30,6 +45,7 @@ def create_two_cycle_graph(
 ) -> nx.MultiDiGraph:
     """
     Create two cycle graph with labels on the edges.
+
     Parameters
     ----------
     first_vertices: int
@@ -38,6 +54,7 @@ def create_two_cycle_graph(
         Amount of vertices in the second cycle
     edge_labels: Tuple[str, str]
         Labels for the edges on the first and second cycle
+
     Returns
     -------
     nx.MultiDiGraph
@@ -46,3 +63,65 @@ def create_two_cycle_graph(
     return cfpq_data.labeled_two_cycles_graph(
         first_vertices, second_vertices, edge_labels=edge_labels, verbose=False
     )
+
+
+def regex_to_nfa(regex: str) -> NondeterministicFiniteAutomaton:
+    """
+    Building a non-deterministic state automaton from a regular expression.
+
+    Parameters
+    ----------
+    regex: str
+        Regular expression.
+
+    Returns
+    -------
+    NondeterministicFiniteAutomaton
+        Non-deterministic Finite Automaton, which is equivalent to given regular expression.
+    """
+
+    rgx = Regex(regex)
+    nfa = rgx.to_epsilon_nfa()
+    return nfa
+
+
+def nfa_to_minimal_dfa(
+    nfa: NondeterministicFiniteAutomaton,
+) -> DeterministicFiniteAutomaton:
+    """
+    Building a non-deterministic state automaton from a regular expression.
+
+    Parameters
+    ----------
+    nfa: NondeterministicFiniteAutomaton
+        Non-deterministic Finite Automaton.
+
+    Returns
+    -------
+    DeterministicFiniteAutomaton
+        Deterministic Finite Automaton, which is equivalent to given non-deterministic Finite Automaton.
+    """
+
+    dfa = nfa.to_deterministic()
+    dfa = dfa.minimize()
+    return dfa
+
+
+def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
+    """
+    Building a non-deterministic state automaton from a regular expression.
+
+    Parameters
+    ----------
+    regex: NondeterministicFiniteAutomaton
+        Non-deterministic Finite Automaton.
+
+    Returns
+    -------
+    DeterministicFiniteAutomaton
+        Deterministic Finite Automaton, which is equivalent to given regex expression.
+    """
+
+    nfa = regex_to_nfa(regex)
+    dfa = nfa_to_minimal_dfa(nfa)
+    return dfa
