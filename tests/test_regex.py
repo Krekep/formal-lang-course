a685670a77f2f30e240c5332994fc140c0e19677
Pavel Alimov <42354122+Krekep@users.noreply.github.com>
2021-09-22 14:23:07 +0300

    [Fix] Fix min dfa test

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -28,6 +28,7 @@ def test_get_min_dfa():
 
     state_0 = State(0)
     state_1 = State(1)
+    state_2 = State(2)
 
     symbol_a = Symbol("a")
     symbol_b = Symbol("b")
@@ -35,14 +36,14 @@ def test_get_min_dfa():
 
     expected_dfa.add_start_state(state_0)
 
-    expected_dfa.add_final_state(state_0)
+    expected_dfa.add_final_state(state_2)
 
     expected_dfa.add_transition(state_0, symbol_a, state_1)
     expected_dfa.add_transition(state_0, symbol_c, state_1)
 
-    expected_dfa.add_transition(state_1, symbol_b, state_0)
+    expected_dfa.add_transition(state_1, symbol_b, state_2)
 
-    actual_dfa = utils.regex_to_dfa("((a | c) b)*")
+    actual_dfa = utils.regex_to_dfa("((a | c) b)")
     assert expected_dfa.is_equivalent_to(actual_dfa)
     assert len(actual_dfa.states) == len(expected_dfa.states)
 
