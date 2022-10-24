4b0529371ff3a479684837c054c1b77ef0f28327
Pavel Alimov <42354122+Krekep@users.noreply.github.com>
2022-10-24 18:46:24 +0300

    Update tests

--- tests/test_wcnf.py
+++ tests/test_wcnf.py
@@ -5,7 +5,7 @@ from project.cfg_utils import cfg_to_wcnf
 
 
 @pytest.mark.parametrize(
-    "cfg, expected",
+    "cfg, expected, contained_word",
     [
         (
             """
@@ -15,6 +15,10 @@ from project.cfg_utils import cfg_to_wcnf
                 S -> S S
                 S -> epsilon
             """,
+            {
+                True: [""],
+                False: ["  ", "S"],
+            },
         ),
         (
             """
@@ -25,14 +29,77 @@ from project.cfg_utils import cfg_to_wcnf
             """
                 S -> b
             """,
+            {
+                True: ["b"],
+                False: ["", "a", "bb"],
+            },
+        ),
+        (
+            """
+                S -> a b c d e
+            """,
+            """
+                S -> A B1
+                B1 -> B2 C1
+                C1 -> C2 D1
+                D1 -> D2 E
+                A -> a
+                B2 -> b
+                C2 -> c
+                D2 -> d
+                E -> e 
+            """,
+            {
+                True: ["abcde"],
+                False: ["abcd", "", "bb"],
+            },
+        ),
+        (
+            """
+                S -> a S b N c
+                S -> epsilon
+                N -> epsilon
+                N -> N N
+                N -> d e f g h
+            """,
+            """
+                S -> epsilon
+                N -> epsilon
+                N -> N N
+                S -> A C4
+                A -> a
+                N -> D C1
+                D -> d
+                C1 -> E C2
+                E -> e
+                C2 -> F C3
+                F -> f
+                C3 -> G H
+                G -> g
+                H -> h
+                C4 -> S C5
+                C5 -> B C6
+                B -> b
+                C6 -> N C
+                C -> c
+            """,
+            {
+                True: ["", "abc", "aabcbc", "aabcbdefghc"],
+                False: ["aabcb", "ab", "bb", "defgh"],
+            },
         ),
     ],
 )
-def test_wcnf(cfg, expected):
+def test_wcnf(cfg, expected, contained_word):
     actual = cfg_to_wcnf(cfg)
     wcnf_expected = CFG.from_text(expected)
     assert actual.start_symbol == wcnf_expected.start_symbol
-    assert actual.productions == wcnf_expected.productions
+    assert all(
+        actual.contains(w) and wcnf_expected.contains(w) for w in contained_word[True]
+    ) and all(
+        not actual.contains(w) and not wcnf_expected.contains(w) for w in contained_word[False]
+    )
+    assert len(actual.productions) == len(wcnf_expected.productions)
 
 
 @pytest.mark.parametrize(
